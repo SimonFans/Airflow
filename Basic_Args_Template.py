95f00b63fabeb0e412d3f8e07c1f87f734d428f4
Simon Zhao <Ximengzhao1220@gmail.com>
2020-04-09 18:14:28 -0700

    Create Basic_Args_Template.py

---
+++ Basic_Args_Template.py
@@ -0,0 +1,20 @@
+from airflow import DAG 
+from datetime import datatime, timedelta 
+
+default_args = {
+    "owner" : "airflow",
+    "start_date" : datetime(2020,4,8),
+    "depend_on_past" : False,
+    "email_on_failure" : False,
+    "email_on_retry" : False,
+    "email" : "ximengzhao1220@gmail.com",
+    "retries" : 1,
+    "retry_delay": timedelta(minutes=5)
+}
+
+dag_id="forex_data_pipeline"
+schedule_interval="@daily"
+
+
+with DAG(dag_id, schedule_interval, default_args=default_args, catchup=False) as dag:
+    None
