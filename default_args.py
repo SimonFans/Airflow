813f48845ca16cbe754c0f341ae426812d86cc56
Simon Zhao <Ximengzhao1220@gmail.com>
2021-08-03 17:50:31 -0700

    Update default_args.py

--- default_args.py
+++ default_args.py
@@ -4,6 +4,7 @@ from airflow.utils.dates import days_ago
 
 # depends_on_past: (boolean) when set to True, keeps a task from getting triggered if the previous schedule for the task hasn’t succeeded.
 # Never make start date to be current date. It won't run the dag because the dag will trigger after (start_date + schedule_interval). You can use days_ago to set up the start_date
+# retry_exponential_backoff use case is multiple calls to one API. It's based on an algorithm to extend time for each retry delay
 
 default_args = {
   'owner': 'airflow',
