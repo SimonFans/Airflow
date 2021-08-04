51120646c4ac92b77b44e457f1993920d6afe160
Simon Zhao <Ximengzhao1220@gmail.com>
2021-08-03 17:18:37 -0700

    Update default_args.py

--- default_args.py
+++ default_args.py
@@ -1,11 +1,14 @@
 from airflow import DAG
 from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
 
 # depends_on_past: (boolean) when set to True, keeps a task from getting triggered if the previous schedule for the task hasn’t succeeded.
+# Never make start date to be current date. It won't run the dag because the dag will trigger after (start_date + schedule_interval). You can use days_ago to set up the start_date
 
 default_args = {
   'owner': 'airflow',
   'start_date': datetime(2020,1,30),
+  # 'start_date': days_ago(5),
   'email': ['xxx@gmail.com'],
   'email_on_failure': True,
   'email_on_retry': True,
