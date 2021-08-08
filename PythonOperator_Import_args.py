e15a7ea7d207d7096478bf4b3aed746e2d4d8e66
Simon Zhao <Ximengzhao1220@gmail.com>
2021-08-08 16:38:59 -0700

    Update PythonOperator_Import_args.py

--- PythonOperator_Import_args.py
+++ PythonOperator_Import_args.py
@@ -6,6 +6,10 @@ from airflow.operators.email_operator import EmailOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from pprint import pprint
+# import task decorator then you don't need to have PythonOperator
+from airflow.decorators import task
+# Below is for import ti when you use task decorator
+from airflow.operators.python import get_current_context
 
 # In the UI variable, define path, and filename. As for my_settings, fill val in {"path": "/opt/local/airflow", "filename": "test.json"}
 
@@ -13,7 +17,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2021, 8, 6),
-    'email': ['xxx@gmail.com'],
+    'email': ['ximengzhao1220@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 1,
@@ -25,6 +29,11 @@ def _process_one(path, filename):
 
 def _process_two(path, filename):
     print(f"{path}/{filename}")
+
+@task(task_id="task_c")
+def _process_three(my_settings):
+    context = get_current_context()
+    print(f"{my_settings['path']}/{my_settings['filename']}-{context['ds']}")
 
 with DAG('my_python_dag', default_args=default_args, schedule_interval= '@daily', catchup=False) as dag:
     # (1) Method 1 to import args
@@ -43,4 +52,9 @@ with DAG('my_python_dag', default_args=default_args, schedule_interval= '@daily'
     op_kwargs=Variable.get("my_settings",deserialize_json=True)
     )
 
-    t1>>t2
+    store = BashOperator(
+        task_id="store",
+        bash_command="echo 'store'"
+    )
+
+    t1 >> t2 >> _process_three(Variable.get("my_settings",deserialize_json=True)) >> store
