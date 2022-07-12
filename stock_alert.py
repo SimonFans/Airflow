73f9f40ad6c6fc608a1e2bb7ff464aa947ed5b01
Simon Zhao <Ximengzhao1220@gmail.com>
2022-07-12 16:43:07 -0700

    Update stock_alert.py

--- stock_alert.py
+++ stock_alert.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, time
 from collections import defaultdict
 from airflow.operators.email_operator import EmailOperator
 from airflow.operators.dummy_operator import DummyOperator
@@ -8,20 +8,23 @@ from airflow.operators.python_operator import PythonOperator
 from pprint import pprint
 from yahoo_fin import stock_info as si
 from airflow.models import Variable
-
+from airflow.operators.datetime import BranchDateTimeOperator
+from airflow.operators.weekday import BranchDayOfWeekOperator
+from airflow.utils.weekday import WeekDay
+import json
 
 default_args = {
-    'owner': 'airflow',
+    'owner': 'Ximeng',
     'depends_on_past': False,
     'start_date': datetime(2021, 8, 15),
-    'email': ['xxx@gmail.com'],
+    'email': ['ximengzhao1220@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=0.5)
 }
 
-dag = DAG('my_stock', default_args=default_args, schedule_interval= '0 */3 * * 1-5', catchup=False)
+dag = DAG('my_stock', default_args=default_args, schedule_interval= '*/30 * * * 1-5', catchup=False)
 
 # Variables
 dag_config = Variable.get("stock_file",deserialize_json=True)
@@ -34,8 +37,9 @@ def get_my_stock_list(**kwargs):
     rm_parenthesis_str = ordered_stock_str.lstrip('{').rstrip('}')
     ordered_stock_lst = rm_parenthesis_str.split(',')
     for each_stock in ordered_stock_lst:
-        stock_name, price = each_stock.split(':')[0].strip(), float(each_stock.split(':')[1])
+        stock_name, price = each_stock.split(':')[0].strip().replace("'", ""), float(each_stock.split(':')[1])
         cleaned_lst.append((stock_name, price))
+    print(cleaned_lst)
     return cleaned_lst
 
 def get_my_stock_name_list(**kwargs):
@@ -53,30 +57,70 @@ def check_stock(**kwargs):
     #name_list=['Lyft', 'Alibaba' 'Airbnb', 'Coupang', 'Delta', 'Zillow', 'Apple', 'Uber']
     name_list = kwargs['ti'].xcom_pull(task_ids='get_my_stock_name_list')
     my_stock_lst = kwargs['ti'].xcom_pull(task_ids='get_my_stock_list')
-    name=['LYFT', 'BABA', 'ABNB', 'CPNG', 'DAL', 'Z', 'AAPL', 'UBER']
+    name=['LYFT', 'BABA', 'CPNG', 'Z','PYPL','AAPL','SNAP']
+    consider_to_all = []
+    for i in range(len(my_stock_lst)):
+        my_stock_price = my_stock_lst[i][1]
+        current_stock_price = parsePrice(name[i])
+        # if my_stock_price <= current_stock_price:
+        consider_to_all.append((my_stock_lst[i][0],my_stock_price,current_stock_price))
+    print(consider_to_all)
+    email_content = []
+    email_key_name =  ["Stock_Name","Your_Purchased_$","Current_Stock_$"]
+    test = []
+    for val in consider_to_all:
+        email_content.append(dict(zip(email_key_name, val)))
+    email_content_json = json.dumps(email_content, indent=4)
+    return email_content_json
+
+def sell_stock(**kwargs):
+    name_list = kwargs['ti'].xcom_pull(task_ids='get_my_stock_name_list')
+    my_stock_lst = kwargs['ti'].xcom_pull(task_ids='get_my_stock_list')
+    name=['LYFT', 'BABA', 'CPNG', 'Z','PYPL','AAPL','SNAP']
     consider_to_sell = []
     for i in range(len(my_stock_lst)):
         my_stock_price = my_stock_lst[i][1]
         current_stock_price = parsePrice(name[i])
         if my_stock_price <= current_stock_price:
-            consider_to_sell.append((my_stock_lst[i][0],my_stock_price,current_stock_price))
-    email_content = []
-    email_key_name =  ['Stock_name','Your_purchased_price','Current_stock_price']
-    for val in consider_to_sell:
-        email_content.append(dict(zip(email_key_name, val)))
-    return email_content
+            consider_to_sell.append(my_stock_lst[i][0])
+    return consider_to_sell
 
+is_in_timeframe = BranchDateTimeOperator(
+        task_id = "is_in_timeframe",
+        follow_task_ids_if_true = ['read_variables'],
+        follow_task_ids_if_false = ['not_in_timeframe'],
+        # If you set time lower > upper, such as lower: 1Pm, upper: 11:00 AM, then 1PM means the next day 1PM, so timeframe becomes today 11AM - 1:00PM (tomorrow)
+        target_lower = time(14,0,0),
+        target_upper = time(21,0,0),
+        # By default, use_task_execution_date is False. But if you want to run past dags, you have to set it to True
+        use_task_execution_date = True,
+        dag=dag
+        )
 
-t1 = BashOperator(
-    task_id='Start',
-    bash_command='echo "Start checking my ordered stock....."',
-    dag=dag)
+is_weekday = BranchDayOfWeekOperator(
+        task_id = "is_weekday",
+        follow_task_ids_if_true = ['is_in_timeframe'],
+        follow_task_ids_if_false = ['non_weekday_task'],
+        week_day = {WeekDay.MONDAY, WeekDay.TUESDAY, WeekDay.WEDNESDAY, WeekDay.THURSDAY, WeekDay.FRIDAY},
+        use_task_execution_day = True,
+        dag=dag
+        )
+
+not_in_timeframe = DummyOperator(
+        task_id = 'not_in_timeframe',
+        dag=dag
+        )
+
+non_weekday_task = DummyOperator(
+        task_id = 'non_weekday_task',
+        dag=dag
+        )
 
 t2 = BashOperator(
     task_id="read_variables",
     bash_command='echo "{0}"'.format(dag_config),
-    dag=dag,
-)
+    dag=dag
+    )
 
 t3 = PythonOperator(
     task_id='get_my_stock_list',
@@ -93,15 +137,25 @@ t5 = PythonOperator(
     python_callable=check_stock,
     dag=dag)
 
+t6 = PythonOperator(
+    task_id='sell_stock',
+    python_callable=sell_stock,
+    dag=dag)
+
 send_email = EmailOperator(
         task_id = 'Send_Email',
-        to = ['xxx@gmail.com'],
-        subject = 'Airflow: Stock Check!!',
+        to = ['ximengzhao1220@gmail.com', 'ritajiaxinwang1233@gmail.com'],
+        #
+        subject = 'Airflow Alert: Ximeng Stock Check!!',
         html_content = """
-                        <p>You may consider to sell: </p>
-                        <p>{{task_instance.xcom_pull(task_ids = 'check_stock')}}<br><br>
+                        <p><mark>Current Stocks Market $: </mark></p>
+                        {{task_instance.xcom_pull(task_ids = 'check_stock')}}<br>
+                        <p><mark>You may consider to sell stock: </mark></p>
+                        <p>{{task_instance.xcom_pull(task_ids = 'sell_stock')}}</p><br><br>
                         """,
         dag = dag
         )
 
-t1 >> t2 >> t3 >> t4 >> t5 >> send_email
+is_weekday >> [is_in_timeframe, non_weekday_task]
+is_in_timeframe >> [t2, not_in_timeframe]
+t2 >> t3 >> t4 >> t5 >> t6 >> send_email
